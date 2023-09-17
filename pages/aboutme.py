f7b8b9cac13e38735ae48cba7b0b38677f6ed149
cerman <jaroslav.cerman@merck.com>
2023-09-17 12:04:28 +0200

    About me page

---
+++ pages/aboutme.py
@@ -0,0 +1,22 @@
+from dash import register_page
+import dash_mantine_components as dmc
+
+register_page(__name__, path='/about-me')
+
+bout_me = """
+I am a data engineer with a background in bioinformatics and a passion for data science.
+"""
+layout = dmc.Grid([
+    dmc.Col([
+        dmc.Grid([
+            dmc.Divider(label='who am I', size='sm'),
+        ]),
+        dmc.Grid([
+            dmc.Col(dmc.Text(bout_me), span=8),
+            dmc.Col(dmc.Image(src='/assets/profile_picture_pingu.svg'), span=4),
+        ]),
+    ], span=6, offset=3)  # default span=12, finer grid can be created by nesting
+])
+
+if __name__ == '__main__':
+    ...
